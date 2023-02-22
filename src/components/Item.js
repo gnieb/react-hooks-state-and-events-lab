8925ae4974688f8720e9673f3ee0882a2a0db38f
Grace Nieboer <grace.nieboer@outlook.com>
2023-02-22 17:10:41 -0500

    filtering events with state

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+
+const [isItemInCart, setIsItemInCart] = useState("");
+const itemClass = isItemInCart ? "in-cart":"";
+const buttonText = isItemInCart ? "Remove From Cart" : "Add to Cart"
+
+const handleItem = () => setIsItemInCart(!isItemInCart)
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleItem}className="add">{buttonText}</button>
     </li>
   );
 }
